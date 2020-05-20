38f70a423dfb686eaccf27cae2d40b3191cf5dc2
Andrey <andrewpltnv@gmail.com>
2020-05-21 00:39:02 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main id="main">
+        
+      </main>
+      <favourite id="favourite">
+
+      </favourite>
+    </div>
+  );
+}
+
+export default App;
